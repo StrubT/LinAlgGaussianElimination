ea3bbc5ae036b65162e51f480d4ceaaf8cd45f74
Thomas Reto Strub <ths@strubt.ch>
2017-01-09 21:46:37 +0100

    commit project

---
+++ GaussianElimination/Math.cs
@@ -0,0 +1,383 @@
+﻿using System;
+using System.Globalization;
+using static System.Math;
+
+namespace StrubT.Buas.LinAlg.Matrix.GaussianElimination {
+
+	/// <summary>
+	/// Event fired after every column elimination step
+	/// </summary>
+	/// <param name="column">Number of the eliminated column</param>
+	public delegate void ColumnEliminationCompleted(int column);
+
+	/// <summary>
+	/// Mathematical fraction
+	/// </summary>
+	public class Fraction : IFormattable, IComparable, IComparable<Fraction>, IEquatable<Fraction> {
+
+		#region properties / constructor
+
+		/// <summary>
+		/// Numerator of the fraction
+		/// </summary>
+		public int Numerator { get; }
+
+		/// <summary>
+		/// Denominator of the fraction
+		/// </summary>
+		public int Denominator { get; }
+
+		/// <summary>
+		/// Constructor
+		/// Create a new fraction with a <c>denominator</c> of 1
+		/// </summary>
+		/// <param name="numerator">Numerator of the fraction</param>
+		public Fraction(int numerator) : this(numerator, 1) { }
+
+		/// <summary>
+		/// Constructor
+		/// Create a new fraction
+		/// </summary>
+		/// <param name="numerator">Numerator of the fraction</param>
+		/// <param name="denominator">Denominator of the fraction</param>
+		public Fraction(int numerator, int denominator) {
+
+			if (denominator == 0) throw new DivideByZeroException("Cannot have a denominator 0.");
+
+			int gcd = Gcd(Abs(numerator), Abs(denominator));
+			//use greatest common divisor to simplify fractions
+
+			//use denominator / Abs(denominator) to apply denominators sign to the numerator (prevent negative denominators)
+			Numerator = numerator * (denominator / Abs(denominator)) / gcd;
+			Denominator = Abs(denominator) / gcd;
+		}
+
+		int Gcd(int a, int b) {
+
+			//EUCLID'S ALGORITHM
+			//source: http://stackoverflow.com/a/2941224/1325979 (shortened)
+			while (b > 0)
+				b = a % (a = b);
+			return a;
+		}
+		#endregion
+
+		#region mathematical operators
+
+		/// <summary>
+		/// Add two fractions
+		/// </summary>
+		/// <param name="f1">First fraction / operand</param>
+		/// <param name="f2">Second fraction / operand</param>
+		/// <returns>Sum of the two fractions as simplified fraction</returns>
+		public static Fraction operator +(Fraction f1, Fraction f2) => new Fraction(f1.Numerator * f2.Denominator + f2.Numerator * f1.Denominator, f1.Denominator * f2.Denominator);
+
+		/// <summary>
+		/// Negate (change sign of) a fraction
+		/// </summary>
+		/// <param name="f">Fraction to negate</param>
+		/// <returns>Negative (or positive in case input was negative) fraction with same absolute value</returns>
+		public static Fraction operator -(Fraction f) => new Fraction(-f.Numerator, f.Denominator);
+
+		/// <summary>
+		/// Subtract one fraction from another
+		/// </summary>
+		/// <param name="f1">First fraction / minuend</param>
+		/// <param name="f2">Second fraction / subtrahend</param>
+		/// <returns>Difference of the two fractions as simplified fraction</returns>
+		public static Fraction operator -(Fraction f1, Fraction f2) => new Fraction(f1.Numerator * f2.Denominator - f2.Numerator * f1.Denominator, f1.Denominator * f2.Denominator);
+
+		/// <summary>
+		/// Multiply two fractions
+		/// </summary>
+		/// <param name="f1">First fraction / factor</param>
+		/// <param name="f2">Second fraction / factor</param>
+		/// <returns>Product of the two fractions as simplified fraction</returns>
+		public static Fraction operator *(Fraction f1, Fraction f2) => new Fraction(f1.Numerator * f2.Numerator, f1.Denominator * f2.Denominator);
+
+		/// <summary>
+		/// Divide one fraction by another
+		/// </summary>
+		/// <param name="f1">First fraction / dividend</param>
+		/// <param name="f2">Second fraction / divisor</param>
+		/// <returns>Quotient of the two fractions as simplified fraction</returns>
+		public static Fraction operator /(Fraction f1, Fraction f2) => new Fraction(f1.Numerator * f2.Denominator, f1.Denominator * f2.Numerator);
+
+		/// <summary>
+		/// Check two fractions for equality
+		/// </summary>
+		/// <param name="f1">First fraction</param>
+		/// <param name="f2">Second fraction</param>
+		/// <returns><c>true</c> if the to fractions numerators and denominators are equal, <c>false</c> otherwise</returns>
+		public static bool operator ==(Fraction f1, Fraction f2) => f1.Numerator == f2.Numerator && f1.Denominator == f2.Denominator;
+
+		/// <summary>
+		/// Check two fractions for inequality
+		/// </summary>
+		/// <param name="f1">First fraction</param>
+		/// <param name="f2">Second fraction</param>
+		/// <returns><c>true</c> if the to fractions numerators or denominators are different, <c>false</c> otherwise</returns>
+		public static bool operator !=(Fraction f1, Fraction f2) => !(f1 == f2);
+
+		/// <summary>
+		/// Check if one fraction is smaller or equal another
+		/// </summary>
+		/// <param name="f1">First fraction</param>
+		/// <param name="f2">Second fraction</param>
+		/// <returns><c>true</c> if first fraction is smaller or equal the second one, <c>false</c> otherwise</returns>
+		public static bool operator <=(Fraction f1, Fraction f2) => f1.Numerator * f2.Denominator <= f2.Numerator * f1.Denominator;
+
+		/// <summary>
+		/// Check if one fraction is smaller another
+		/// </summary>
+		/// <param name="f1">First fraction</param>
+		/// <param name="f2">Second fraction</param>
+		/// <returns><c>true</c> if first fraction is smaller the second one, <c>false</c> otherwise</returns>
+		public static bool operator <(Fraction f1, Fraction f2) => f1.Numerator * f2.Denominator < f2.Numerator * f1.Denominator;
+
+		/// <summary>
+		/// Check if one fraction is bigger or equal another
+		/// </summary>
+		/// <param name="f1">First fraction</param>
+		/// <param name="f2">Second fraction</param>
+		/// <returns><c>true</c> if first fraction is bigger or equal the second one, <c>false</c> otherwise</returns>
+		public static bool operator >=(Fraction f1, Fraction f2) => f1.Numerator * f2.Denominator >= f2.Numerator * f1.Denominator;
+
+		/// <summary>
+		/// Check if one fraction is bigger another
+		/// </summary>
+		/// <param name="f1">First fraction</param>
+		/// <param name="f2">Second fraction</param>
+		/// <returns><c>true</c> if first fraction is bigger the second one, <c>false</c> otherwise</returns>
+		public static bool operator >(Fraction f1, Fraction f2) => f1.Numerator * f2.Denominator > f2.Numerator * f1.Denominator;
+		#endregion
+
+		#region casting operators
+
+		/// <summary>
+		/// Cast an integer number to a fraction
+		/// </summary>
+		/// <param name="i">Integer to use as numerator of fraction</param>
+		/// <returns>A fraction with <c>i</c> as its numerator and <c>1</c> as its denominator</returns>
+		public static implicit operator Fraction(int i) => new Fraction(i);
+
+		/// <summary>
+		/// Cast a fraction to a floating point number
+		/// </summary>
+		/// <param name="f">Fraction to cast</param>
+		/// <returns>The quotient of the fraction's numerator and denominator as a floating-point number</returns>
+		public static implicit operator double(Fraction f) => (double)f.Numerator / f.Denominator;
+		#endregion
+
+		#region comparison
+
+		/// <summary>
+		/// Generate a unique hash code for the fraction
+		/// </summary>
+		/// <returns>A hash code based on the fraction's numerator and denominator</returns>
+		public override int GetHashCode() => ((17 + Numerator) * 37 + Denominator) * 37;
+
+		/// <summary>
+		/// Check if a second fraction equals this one
+		/// </summary>
+		/// <param name="obj">Second fraction (or any object, really)</param>
+		/// <returns>If second object is a fraction and the fraction's numerator and denominator equal this ones</returns>
+		public override bool Equals(object obj) => obj is Fraction && Equals((Fraction)obj);
+
+		/// <summary>
+		/// Check if a second fraction equals this one
+		/// </summary>
+		/// <param name="other">Second fraction</param>
+		/// <returns>If second fraction's numerator and denominator equal this ones</returns>
+		public bool Equals(Fraction other) => other != null && Numerator == other.Numerator && Denominator == other.Denominator;
+
+		/// <summary>
+		/// Compare this fraction to an other one
+		/// </summary>
+		/// <param name="obj">Second fraction (or any object, really)</param>
+		/// <returns>A negative number (<c>-1</c>) if this fraction is smaller the second one, <c>0</c> if they are equal and a positive number (<c>1</c>) if this one is bigger the second one</returns>
+		int IComparable.CompareTo(object obj) => obj is Fraction ? CompareTo((Fraction)obj) : -1;
+
+		/// <summary>
+		/// Compare this fraction to an other one
+		/// </summary>
+		/// <param name="other">Second fraction</param>
+		/// <returns>A negative number (<c>-1</c>) if this fraction is smaller the second one, <c>0</c> if they are equal and a positive number (<c>1</c>) if this one is bigger the second one</returns>
+		public int CompareTo(Fraction other) => other != null ? (Numerator * other.Denominator).CompareTo(other.Numerator * Denominator) : 1;
+		#endregion
+
+		#region string representation
+
+		/// <summary>
+		/// Get a string representation of this fraction
+		/// </summary>
+		/// <returns>A string representation of this fraction's numerator and denominator</returns>
+		public override string ToString() => ToString(null, null);
+
+		/// <summary>
+		/// Get a string representation of this fraction
+		/// </summary>
+		/// <param name="format">Format of the string representation (ignored)</param>
+		/// <returns>A string representation of this fraction's numerator and denominator</returns>
+		public string ToString(string format) => ToString(format, null);
+
+		/// <summary>
+		/// Get a string representation of this fraction
+		/// </summary>
+		/// <param name="formatProvider">Format provider (culture) to use when formatting the numbers</param>
+		/// <returns>A string representation of this fraction's numerator and denominator</returns>
+		public string ToString(IFormatProvider formatProvider) => ToString(null, formatProvider);
+
+		/// <summary>
+		/// Get a string representation of this fraction
+		/// </summary>
+		/// <param name="format">Format of the string representation (ignored)</param>
+		/// <param name="formatProvider">Format provider (culture) to use when formatting the numbers</param>
+		/// <returns>A string representation of this fraction's numerator and denominator</returns>
+		public string ToString(string format, IFormatProvider formatProvider) => string.Format(formatProvider ?? CultureInfo.CurrentCulture, Denominator == 1 ? "{0:#,##0}" : "{0:#,##0}/{1:#,##0}", Numerator, Denominator);
+		#endregion
+	}
+
+	/// <summary>
+	/// Mathematical matrix (as used in the Gaussian elimination)
+	/// </summary>
+	public class Matrix {
+
+		/// <summary>
+		/// Event fired after every column elimination step
+		/// </summary>
+		public event ColumnEliminationCompleted ColumnEliminationCompleted;
+
+		#region properties
+
+		/// <summary>
+		/// Number of rows / equations in the matrix
+		/// </summary>
+		public int NofRows => matrix.GetLength(0);
+
+		/// <summary>
+		/// Number of rows / equations in the matrix
+		/// </summary>
+		public int NofEquations => NofRows;
+
+		/// <summary>
+		/// Number of columns in the matrix
+		/// </summary>
+		public int NofColumns => matrix.GetLength(0);
+
+		/// <summary>
+		/// Number of variables in the matrix
+		/// </summary>
+		public int NofVariables => NofColumns - 1;
+
+		readonly Fraction[,] matrix;
+		#endregion
+
+		#region constructors
+
+		/// <summary>
+		/// Constructor
+		/// Create an empty matrix of size <c>size</c>
+		/// </summary>
+		/// <param name="size">Size of the matrix (number of equations or rows)</param>
+		public Matrix(int size) : this(size, size) { }
+
+		/// <summary>
+		/// Constructor
+		/// Create an empty matrix of size <c>nofEquations</c> x <c>nofVariables + 1</c>
+		/// </summary>
+		/// <param name="nofEquations">Number of rows / equations in the matrix</param>
+		/// <param name="nofVariables">Number of variables in the matrix</param>
+		public Matrix(int nofEquations, int nofVariables) {
+
+			matrix = new Fraction[nofEquations, nofVariables + 1];
+		}
+
+		/// <summary>
+		/// Constructor
+		/// Wrap a predefined matrix
+		/// </summary>
+		/// <param name="matrix">Values of the predefined matrix</param>
+		internal Matrix(Fraction[,] matrix) {
+
+			this.matrix = matrix;
+		}
+		#endregion
+
+		#region instantiation
+
+		/// <summary>
+		/// Value at position <c>row</c>,<c>col</c>
+		/// </summary>
+		/// <param name="row">Row index (starts with <c>0</c>)</param>
+		/// <param name="col">Column index (starts with <c>0</c>)</param>
+		/// <returns>Value at position <c>row</c>,<c>col</c></returns>
+		public Fraction this[int row, int col] {
+			set { matrix[row, col] = value; }
+			get { return matrix[row, col]; }
+		}
+		#endregion
+
+		#region elimination
+
+		/// <summary>
+		/// Run the Gaussian elimination algorithm
+		/// </summary>
+		public void Eliminate() {
+
+			for (int rc = 0; rc < NofVariables && rc < NofRows; rc++) {
+
+				//1) bestimme weitesten links stehende Spalte mit min. einer Zeile =/= 0
+				var skip = true;
+				for (int r = 0; skip && r < NofRows; r++)
+					if (this[r, rc] != 0) skip = false;
+				if (skip) continue;
+
+				//2) falls nötig, Zeilen vertauschen
+				if (this[rc, rc] == 0) {
+					skip = true;
+					for (int r = rc + 1; skip && r < NofRows; r++)
+						if (this[r, rc] != 0) {
+							SwapRows(rc, r);
+							skip = false;
+						}
+					if (skip) continue;
+				}
+
+				//3) falls das 1. Element der Stufe a ist, mit 1/a multiplizieren
+				MultiplyRow(rc, 1 / this[rc, rc]);
+
+				//4) passende Vielfache der 1. Zeile addieren, um unter der führenden Eins Nullen zu erhalten
+				for (int j = 0; j < NofRows; j++) {
+					if (rc == j) continue;
+
+					AddRow(j, -this[j, rc] / this[rc, rc], rc);
+				}
+
+				ColumnEliminationCompleted?.Invoke(rc);
+			}
+		}
+
+		void SwapRows(int d, int s) {
+
+			for (int c = 0; c < NofColumns; c++) {
+				var t = this[d, c];
+				this[d, c] = this[s, c];
+				this[s, c] = t;
+			}
+		}
+
+		void MultiplyRow(int r, Fraction f) {
+
+			for (int c = 0; c < NofColumns; c++)
+				this[r, c] *= f;
+		}
+
+		void AddRow(int d, Fraction f, int s) {
+
+			for (int c = 0; c < NofColumns; c++)
+				this[d, c] += f * this[s, c];
+		}
+		#endregion
+	}
+}
---
+++ GaussianElimination/Program.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Linq;
+using c = System.Console;
+using cc = System.ConsoleColor;
+
+namespace StrubT.Buas.LinAlg.Matrix.GaussianElimination {
+
+	static class Program {
+
+		static void Main() {
+
+			//testFractions();
+			TestEliminatePredef();
+			//testEliminateRandom();
+		}
+
+		static void TestFractions() {
+
+			c.WriteLine("*** FRACTION TEST ***");
+
+			var f1 = new Fraction(5);
+			var f2 = new Fraction(-5);
+			var f3 = new Fraction(5, 1);
+			var f4 = new Fraction(-5, 1);
+			var f5 = new Fraction(5, -1);
+			var f6 = new Fraction(5, 2);
+			var f7 = new Fraction(17, 18);
+			var f8 = new Fraction(225, 15);
+			var f9 = new Fraction(-99, -18);
+
+			c.WriteLine("{0} == {1}: {2}", f1, f2, f1 == f2);
+			c.WriteLine("{0} == -{1}: {2}", f1, f2, f1 == -f2);
+			c.WriteLine("{0} == {1}: {2}", f1, f3, f1 == f3);
+			c.WriteLine("{0} == {1}: {2}", f2, f4, f2 == f4);
+			c.WriteLine("{0} == {1}: {2}", f2, f5, f2 == f5);
+
+			c.WriteLine("{0} + {1}: {2}", f1, f3, f1 + f3);
+			c.WriteLine("{0} - {1}: {2}", f1, f3, f1 - f3);
+
+			c.WriteLine("{0} + {1}: {2}", f1, f2, f1 + f2);
+			c.WriteLine("{0} - {1}: {2}", f1, f2, f1 - f2);
+
+			c.WriteLine("{0} + {1}: {2}", f1, f6, f1 + f6);
+			c.WriteLine("{0} - {1}: {2}", f1, f6, f1 - f6);
+			c.WriteLine("{0} * {1}: {2}", f1, f6, f1 * f6);
+			c.WriteLine("{0} / {1}: {2}", f1, f6, f1 / f6);
+
+			c.WriteLine("{0}", f7);
+			c.WriteLine("{0}", f8);
+			c.WriteLine("{0}", f9);
+		}
+
+		static void TestEliminatePredef() {
+
+			c.WriteLine("*** PREDEFINED ELIMINATION TEST ***");
+
+			TestEliminate(new Fraction[,] {
+				{ 3, 2, -3 },
+				{ -2, 8, 4 }
+			});
+
+			TestEliminate(new Fraction[,] {
+				{ 1, 1, 2, 9 },
+				{ 2, 4, -3, 1 },
+				{ 3, 6, -5, 0 }
+			});
+
+			TestEliminate(new Fraction[,] {
+				{ 0, 1, 8, 0, 0, 4 },
+				{ 0, 0, 0, 1, 0, 3 },
+				{ 0, 0, 0, 0, 1, 1 }
+			});
+
+			TestEliminate(new Fraction[,] {
+				{ 1, 2, 3, 4 },
+				{ 0, 1, 5, 6 },
+				{ 0, 0, 1, 7 }
+			});
+
+			TestEliminate(new Fraction[,] {
+				{ 0, 0, -2, 0, 7, 12 },
+				{ 2, 4, -10, 6, 12, 28 },
+				{ 2, 4, -5, 6, -5, -1 }
+			});
+
+			TestEliminate(new Fraction[,] {
+				{ 1, 0, 0, 4, -1 },
+				{ 0, 1, 0, 2, 6 },
+				{ 0, 0, 1, 3, 2 }
+			});
+
+			TestEliminate(new Fraction[,] {
+				{ 1, 6, 0, 0, 4, -2 },
+				{ 0, 0, 1, 0, 3, 1 },
+				{ 0, 0, 0, 1, 5, 2 },
+				{ 0, 0, 0, 0, 0, 0 }
+			});
+
+			TestEliminate(new Fraction[,] {
+				{ 1, 0, 0, 0 },
+				{ 0, 1, 2, 0 },
+				{ 0, 0, 0, 1 }
+			});
+
+			TestEliminate(new Fraction[,] {
+				{ 1, 2, 3, 4, 1 },
+				{ 0, 0, 1, -1, 1 },
+				{ 0, 0, 0, 1, 4 }
+			});
+
+			TestEliminate(new Fraction[,] {
+				{ 3, 5, 1, 2, 1 },
+				{ 2, -4, 3, 7, 2 },
+				{ 4, 14, -1, -3, 0 },
+				{ 13, 7, 9, 20, 7 }
+			});
+		}
+
+		static void TestEliminateRandom() {
+
+			c.WriteLine("*** RANDOM ELIMINATION TEST ***");
+
+			const int EQUATIONS_VARIABLES_MIN = 1;
+			const int EQUATIONS_VARIABLES_MAX = 10;
+
+			const int VALUE_MIN = 1;
+			const int VALUE_MAX = 7;
+
+			var r = new Random();
+			var equations = r.Next(EQUATIONS_VARIABLES_MIN, EQUATIONS_VARIABLES_MAX);
+			var variables = r.Next(EQUATIONS_VARIABLES_MIN, EQUATIONS_VARIABLES_MAX);
+
+			var m = new Matrix(equations, variables);
+			for (int e = 0; e < equations; e++)
+				for (int v = 0; v <= variables; v++)
+					m[e, v] = new Fraction(r.Next(VALUE_MIN, VALUE_MAX));
+
+			TestEliminate(m);
+		}
+
+		static void TestEliminate(Fraction[,] m) {
+
+			TestEliminate(new Matrix(m));
+		}
+
+		static void TestEliminate(Matrix m) {
+
+			var f = c.ForegroundColor;
+			var l = new object();
+
+			c.ForegroundColor = cc.Yellow;
+			c.WriteLine("\n\n\n*** {0} EQUATION{1}, {2} VARIABLE{3}, BEFORE ELIMINATION ***", m.NofEquations, m.NofEquations != 1 ? "S" : null, m.NofVariables, m.NofVariables != 1 ? "S" : null);
+			c.ForegroundColor = f;
+			Print(m);
+
+			m.ColumnEliminationCompleted += col => {
+				lock (l) {
+					c.ForegroundColor = cc.DarkGray;
+					c.WriteLine("\n*** {0} EQUATION{1}, {2} VARIABLE{3}, AFTER COLUMN {4} ***", m.NofEquations, m.NofEquations != 1 ? "S" : null, m.NofVariables, m.NofVariables != 1 ? "S" : null, col + 1);
+					Print(m);
+					c.ForegroundColor = f;
+				}
+			};
+			m.Eliminate();
+
+			c.ForegroundColor = cc.Green;
+			c.WriteLine("\n*** {0} EQUATION{1}, {2} VARIABLE{3}, AFTER ELIMINATION ***", m.NofEquations, m.NofEquations != 1 ? "S" : null, m.NofVariables, m.NofVariables != 1 ? "S" : null);
+			c.ForegroundColor = f;
+			Print(m);
+		}
+
+		static void Print(Matrix m) {
+
+			var s = new string[m.NofRows][];
+			for (int i = 0; i < m.NofRows; i++)
+				s[i] = new string[m.NofColumns];
+
+			var l = 0;
+			for (int i = 0; i < m.NofRows; i++)
+				for (int j = 0; j < m.NofColumns; j++) {
+					s[i][j] = m[i, j].ToString();
+					if (l < s[i][j].Length) l = s[i][j].Length;
+				}
+
+			var f = string.Format("{{0,{0}}}", l);
+
+			c.WriteLine("---{0}---", new string(' ', (l + 2) * m.NofColumns - 2));
+			for (int i = 0; i < m.NofRows; i++)
+				c.WriteLine("| {0} |", string.Join("  ", s[i].Select((n, j) => string.Format((j == m.NofVariables ? "| " : null) + f, n))));
+			c.WriteLine("---{0}---", new string(' ', (l + 2) * m.NofColumns - 2));
+		}
+	}
+}
